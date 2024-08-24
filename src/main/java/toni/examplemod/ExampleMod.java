dd565faf2ef9d4b60431392c1606bcc3576cb7b6
Toni <67132971+nthxny@users.noreply.github.com>
2024-08-24 09:12:51 -0400

    Initial commit

---
+++ src/main/java/toni/examplemod/ExampleMod.java
@@ -0,0 +1,91 @@
+package toni.examplemod;
+
+import toni.examplemod.foundation.config.AllConfigs;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+
+#if FABRIC
+    import net.fabricmc.api.ClientModInitializer;
+    import net.fabricmc.api.ModInitializer;
+    #if after_21_1
+    import fuzs.forgeconfigapiport.fabric.api.neoforge.v4.NeoForgeConfigRegistry;
+    #endif
+
+    #if current_20_1
+    import fuzs.forgeconfigapiport.api.config.v2.ForgeConfigRegistry;
+    #endif
+#endif
+
+#if FORGE
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.Mod.EventBusSubscriber;
+import net.minecraftforge.fml.ModLoadingContext;
+import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+#endif
+
+
+#if NEO
+import net.neoforged.fml.common.Mod;
+import net.neoforged.fml.event.lifecycle.FMLClientSetupEvent;
+import net.neoforged.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.neoforged.bus.api.IEventBus;
+import net.neoforged.fml.ModContainer;
+import net.neoforged.fml.common.EventBusSubscriber;
+#endif
+
+
+#if FORGELIKE
+@Mod("example_mod")
+#endif
+public class ExampleMod #if FABRIC implements ModInitializer, ClientModInitializer #endif
+{
+    public static final String MODNAME = "Example Mod";
+    public static final String MODID = "example_mod";
+    public static final Logger LOGGER = LogManager.getLogger(MODNAME);
+
+    public ExampleMod(#if NEO IEventBus modEventBus, ModContainer modContainer #endif) {
+        #if FORGE
+        var context = FMLJavaModLoadingContext.get();
+        var modEventBus = context.getModEventBus();
+        #endif
+
+        #if FORGELIKE
+        modEventBus.addListener(this::commonSetup);
+        modEventBus.addListener(this::clientSetup);
+        #endif
+
+        AllConfigs.register((type, spec) -> {
+            #if FORGE
+            ModLoadingContext.get().registerConfig(type, spec);
+            #elif NEO
+            modContainer.registerConfig(type, spec);
+            #elif FABRIC
+                #if AFTER_21_1
+                NeoForgeConfigRegistry.INSTANCE.register(ExampleMod.MODID, type, spec);
+                #else
+                ForgeConfigRegistry.INSTANCE.register(ExampleMod.MODID, type, spec);
+                #endif
+            #endif
+        });
+    }
+
+
+    #if FABRIC @Override #endif
+    public void onInitialize() {
+
+    }
+
+    #if FABRIC @Override #endif
+    public void onInitializeClient() {
+
+    }
+
+    // Forg event stubs to call the Fabric initialize methods, and set up cloth config screen
+    #if FORGELIKE
+    public void commonSetup(FMLCommonSetupEvent event) { onInitialize(); }
+    public void clientSetup(FMLClientSetupEvent event) { onInitializeClient(); }
+    #endif
+}
